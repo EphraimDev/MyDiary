920060d3a23fdec415b8c79ebb0f5fb94519c2a4
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-07-20 14:26:26 +0100

    made additions to utils and package.json

--- server/utils/moment.js
+++ server/utils/moment.js
@@ -2,3 +2,5 @@ import moment from 'moment';
 
 export const date = moment().format('L');
 export const time = moment().format('LT');
+export const updatedAt = moment().format();
+export const createdAt = moment().format();
